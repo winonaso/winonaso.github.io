055a20c4a1ef09b52320f4864169d413733cfd42
winonaso <lair@Winonas-MacBook-Pro.local>
2015-01-04 19:59:02 -0800

    All New Portfolio

---
+++ js/js.js
@@ -0,0 +1,117 @@
+(function () {
+	var navBarSticky = new Waypoint.Sticky({
+			element: $("#navi")[0]
+		}),
+		headers = $('img', '#header'),
+		navbarHeight = $("#navi").height(),
+		body = $('body'),
+		scrollToElement = function (element) {
+			body.animate({
+				scrollTop: element.offset().top - navbarHeight
+			}, 500);
+		},
+		projectSection = $('#projects'),
+		projectImageIndex = function (project, index) {
+			var containerWidth = $('.main-image-container', project).width(),
+				mainImage = $('.main-image', project),
+				thumbnails = $('.thumbnails img', project);
+			mainImage.css('transform', 'translateX(' + -1 * index * containerWidth + 'px)');
+			thumbnails.removeClass('selected').eq(index).addClass('selected');
+
+			if (index === 0) {
+				$('.left-arrow', project).addClass('disabled');
+			} else {
+				$('.left-arrow', project).removeClass('disabled');
+			}
+
+			if (index === thumbnails.length - 1 || thumbnails.length === 0) {
+				$('.right-arrow', project).addClass('disabled');
+			} else {
+				$('.right-arrow', project).removeClass('disabled');
+			}
+		};
+
+	headers
+		.addClass('instant')
+		.removeClass('selected')
+		.eq(Math.floor(Math.random()*headers.length))
+		.addClass('selected');
+
+	setTimeout(function () {
+		headers.removeClass('instant');
+	}, 100);
+
+	setInterval(function () {
+		var selected = $('img.selected', '#header').removeClass('selected');
+		headers.eq((headers.index(selected) + 1) % headers.length).addClass('selected');
+	}, 15000);
+
+	projectSection.height(22+$('.project.selected', projectSection).height());
+	$('.project', projectSection).each(function () {
+		projectImageIndex($(this), 0);
+	});
+
+	$('a[href^="#"]').on('click', function (e) {
+		var self = $(this),
+			target = $(self.attr('href'));
+		e.preventDefault();
+		scrollToElement(target);
+	});
+
+	$('#projects').on('click', '.thumbnails img', function () {
+		var self = $(this),
+			project = self.parents('.project');
+
+		projectImageIndex(project, self.index());
+	});
+
+	$('#projects').on('click', '.left-arrow', function () {
+		var self = $(this),
+			project = self.parents('.project'),
+			selected = $('.thumbnails img.selected', project);
+
+		if (self.hasClass('disabled')) {
+			return;
+		}
+
+		projectImageIndex(project, selected.index() - 1);
+	});
+
+	$('#projects').on('click', '.right-arrow', function () {
+		var self = $(this),
+			project = self.parents('.project'),
+			selected = $('.thumbnails img.selected', project);
+
+		if (self.hasClass('disabled')) {
+			return;
+		}
+
+		projectImageIndex(project, selected.index() + 1);
+	});
+
+	$('#project-select').on('click', 'img', function () {
+		var self = $(this),
+			projects = $('#projects .project'),
+			project = projects.eq(self.index());
+		projects.removeClass('selected');
+		project.addClass('selected');
+		projectSection.height(22+project.height());
+		scrollToElement(projectSection);
+	});
+
+	$('#project-select .categories').on('click', '.category', function () {
+		var self = $(this),
+			categoryName = self.data('category-name'),
+			categories = $('#project-select .categories .category');
+
+		categories.removeClass('selected');
+		self.addClass('selected');
+
+		if (categoryName === 'all') {
+			$('#project-select img').removeClass('hide');
+		} else {
+			$('#project-select img').addClass('hide');
+			$('#project-select img.'+categoryName).removeClass('hide');
+		}
+	});
+}());
