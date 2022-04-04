e6c0686c9c6c0c00440617b4c32a0fc18a03e9b8
winonaso <winonaso@users.noreply.github.com>
2022-04-03 21:42:16 -0400

    2022 Portfolio Upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function getFactoid(){
+
+var ar = new Array (8)
+ar[0] = 'I also like mechanical keyboards. <i class="fas fa-keyboard"></i>'
+ar[1] = 'I also love to play indie video games. <i class="fas fa-gamepad-alt"></i>'
+ar[2] = 'I also have a chihuahua mix named Scotch. <i class="fas fa-dog"></i>'
+ar[3] = 'I also have fostered dogs before. <i class="fas fa-bone"></i>'
+ar[4] = 'I also love PB&J. <i class="fas fa-bread-loaf"></i>'
+ar[5] = 'I also have 6 tattoos. <i class="fas fa-flower"></i>'
+ar[6] = 'I also have a 3D printer. <i class="fas fa-cube"></i>'
+
+var now = new Date()
+var sec = now.getSeconds()
+document.getElementById('factoid').innerHTML = ar[sec % 8]
+}
+getFactoid()
